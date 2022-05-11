02317cd08ab995dbe7dc59a4eb2ecc5669ed23a8
Jordan McLeod <jordan.mcleod@15gifts.co.uk>
2022-05-11 08:52:47 +0100

    added leap 2 vector info and csv export

--- gsm_scraping.py
+++ gsm_scraping.py
@@ -22,7 +22,7 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 # -------------------------------------- Finding device page -----------------------------------------------#
 
 driver.get(url)
-time.sleep(3)
+time.sleep(5)
 
 # If a popup appears that requests you to accept cookies, this will accept, if it doesn't appear this step will skip.
 try:
@@ -52,23 +52,34 @@ time.sleep(3)
 
 # ------------------------ All config for devices ---------------------------------------------------#
 
+# -------------------------------------- 5g -----------------------------------------------#
+
+if "5G" in driver.find_element(by=By.XPATH, value='//*[@id="specs-list"]/table[1]/tbody/tr[1]/td[2]/a').text:
+    five_g = 1
+    five_g_vector = "Ready for super-fast <strong>5G</strong> data"
+else:
+    five_g = 0
+    five_g_vector = ""
+
 # -------------------------------------- Release Year and Quarter -----------------------------------------------#
 
 release_year_data = driver.find_element(by=By.XPATH, value='//*[@id="specs-list"]/table[2]/tbody/tr[2]/td[2]').text
 release_year = re.search('[0-9][0-9][0-9][0-9]', release_year_data)
 
-release_quarter_data = driver.find_element(by=By.XPATH, value='//*[@id="specs-list"]/table[2]/tbody'
-                                                              '/tr[2]/td[2]').text.lower()
+# Release Quarter Data
+rqd = driver.find_element(
+    by=By.XPATH,
+    value='//*[@id="specs-list"]/table[2]/tbody/tr[2]/td[2]').text.lower()
 
 release_quarter = 0
 
-if "january" or "february" or "march" in release_quarter_data:
+if "january" in rqd or "february" in rqd or "march" in rqd:
     release_quarter = 1
-elif "april" or "may" or "june" in release_quarter_data:
+elif "april" in rqd or "may" in rqd or "june" in rqd:
     release_quarter = 2
-elif "july" or "august" or "september" in release_quarter_data:
+elif "july" in rqd or "august" in rqd or "september" in rqd:
     release_quarter = 3
-elif "october" or "november" or "december" in release_quarter_data:
+elif "october" in rqd or "november" in rqd or "december" in rqd:
     release_quarter = 4
 
 # -------------------------------------- Operating System -----------------------------------------------#
@@ -79,7 +90,7 @@ try:
         by=By.XPATH,
         value='//*[@id="specs-list"]/table[5]/tbody/tr[1]/td[2]').text
 
-    operating_system = operating_system_data.split(",")[0]
+    operating_system = operating_system_data.split(",")[0][:-2]
 
     # if statement changes amends the text to be lowercase for the Attribute Model
     if "Android" in operating_system_data:
@@ -100,12 +111,43 @@ dimensions_data = driver.find_element(by=By.XPATH, value='//*[@id="specs-list"]/
 weight_data = driver.find_element(by=By.XPATH, value='//*[@id="specs-list"]/table[3]/tbody/tr[2]/td[2]').text
 
 # captures the measurements in mm and the weight in grams
-dimensions_uk = dimensions_data.split("(")[0]
-weight_uk = weight_data.split("(")[0]
+if "Unfolded" in dimensions_data.split("(")[0]:
+    dimensions_uk = dimensions_data.split("(")[0].replace("Unfolded: ", "")
+else:
+    dimensions_uk = dimensions_data.split("(")[0]
+
+# Some UK weights have 2 weights that are a gram apart, this checks if the device has more than one
+# weight and selects the first
+if "or" in weight_data:
+    weight_uk = weight_data.split("(")[0].split("or")[0]
+else:
+    weight_uk = weight_data.split("(")[0]
 
 # captures the measurements in inches and the weight in oz
-dimensions_us = dimensions_data.split("(")[1]
+try:
+    dimensions_us = dimensions_data.split("(")[1]
+except IndexError:
+    dimensions_us = ""
+
 weight_us = re.search('[0-9][.][0-9][0-9][ ][o][z]', weight_data)
+
+
+# ------------------------------------------------ Water resistance ---------------------------------------------------#
+# Catches the values of the Water resistance, if ip is in the text it will gather the data and format like : ipXX
+# otherwise if the phrase water-repellent coating is caught it will add that, alternatively 0 will be returned.
+
+try:
+    water_resistance_data = driver.find_element(
+        by=By.XPATH,
+        value='//*[@id="specs-list"]/table[3]/tbody/tr[4]/td[2]').text.lower()
+    if "ip" in water_resistance_data:
+        water_resistance = re.search('[i][p][0-9][0-9]', water_resistance_data).group()
+    elif "water-repellent coating" in water_resistance_data:
+        water_resistance = "water-repellent coating"
+    else:
+        water_resistance = 0
+except NoSuchElementException:
+    water_resistance = 0
 
 # ------------------------------------------------ Touchscreen --------------------------------------------------------#
 
@@ -140,6 +182,12 @@ ppi = driver.find_element(
 
 camera = driver.find_element(by=By.XPATH, value='//*[@id="specs-list"]/table[7]/tbody/tr[1]/td[1]/a').text
 
+# Gathers info for attribute model advanced info, dual camera if yes 1, no 0
+if camera == "Single":
+    dual_camera = 0
+else:
+    dual_camera = 1
+
 # Gets the number of MP per camera and adds them to the camera section above
 
 camera_data = driver.find_element(
@@ -164,16 +212,48 @@ if driver.find_element(by=By.XPATH, value='//*[@id="specs-list"]/table[8]/tbody/
     front_camera = driver.find_element(
         by=By.XPATH,
         value='//*[@id="specs-list"]/table[8]/tbody/tr[1]/td[2]').text.split(",")[0]
+    front_camera_attribute = front_camera.split(" ")[0]
 else:
     front_camera = driver.find_element(
         by=By.XPATH,
-        value='//*[@id="specs-list"]/table[8]/tbody/tr[1]/td[1]/a').text[:-2]
+        value='//*[@id="specs-list"]/table[8]/tbody/tr[1]/td[1]/a').text
     front_cameras = driver.find_element(
         by=By.XPATH,
         value='//*[@id="specs-list"]/table[8]/tbody/tr[1]/td[2]').text.split("\n")
     for cam in front_cameras:
         split = cam.split(",")
         front_camera += f" {split[0]} +"
+
+    front_camera_attribute = driver.find_element(
+            by=By.XPATH,
+            value='//*[@id="specs-list"]/table[8]/tbody/tr[1]/td[2]').text.split(",")[0].split(" ")[0]
+
+# No devices I could find have a selfie flash, hardcoded to 0
+front_flash = 0
+
+# -------------------------------------- OIS  -------------------------------------------#
+
+ois_data = driver.find_element(by=By.XPATH, value='//*[@id="specs-list"]/table[7]/tbody/tr[1]/td[2]').text.lower()
+if "ois" in ois_data:
+    ois = 1
+else:
+    ois = 0
+
+# -------------------------------------- Camera Aperture  -------------------------------------------#
+
+# Looking for f/X.X if it cannot find this it will set main_camera_aperture to ""
+
+try:
+    main_camera_aperture = re.search('[f][/][0-9][.][0-9]', ois_data).group()
+except NoSuchElementException:
+    main_camera_aperture = ""
+
+# -------------------------------------- Pixel Size  -------------------------------------------#
+
+try:
+    pixel_size = re.search('[0-9][.][0-9][µ][m]', ois_data).group()[:-2]
+except NoSuchElementException:
+    pixel_size = ""
 
 # -------------------------------------- Video Recording  -------------------------------------------#
 
@@ -337,13 +417,29 @@ else:
 
 if "yes" in driver.find_element(by=By.XPATH, value='//*[@id="specs-list"]/table[10]/tbody/tr[4]/td[2]').text.lower():
     other = "NFC"
+    nfc = 1
 else:
     other = ""
+    nfc = 0
 
 # -------------------------------------- Battery  -------------------------------------------------------------#
 
 battery_data = driver.find_element(by=By.XPATH, value='//*[@id="specs-list"]/table[12]/tbody/tr[1]/td[2]').text
 battery = re.search(r'\b\d+\b', battery_data)
+
+# -------------------------------------- Wireless Charging -------------------------------------------------#
+# Checks to see if the device has wireless charging, if so 1 otherwise 0
+
+try:
+    wireless_charging_data = driver.find_element(
+        by=By.XPATH,
+        value='//*[@id="specs-list"]/table[12]/tbody/tr[2]/td[2]').text
+    if "wireless charging" in wireless_charging_data.lower():
+        wireless_charging = 1
+    else:
+        wireless_charging = 0
+except NoSuchElementException:
+    wireless_charging = 0
 
 # -------------------------------------- CSV Export of data collected -------------------------------------------------#
 
@@ -479,9 +575,40 @@ attribute_model_handset = [
     ""
 ]
 
+# --------------------------------- Attribute Model Advanced Handset Data----------------------------------------------#
+
+attribute_model_handset_advanced = [
+    phone_chosen,
+    water_resistance,
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    wireless_charging,
+    nfc,
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    front_flash,
+    ois,
+    "",
+    pixel_size,
+    dual_camera,
+    display_type,
+    "",
+    five_g
+]
+
+
 # --------------------------------- Complete config list -------------------------------------------------------#
 
-complete_config = [device_configuration_uk, device_configuration_us, attribute_model_handset]
+complete_config = [device_configuration_uk, device_configuration_us,
+                   attribute_model_handset, attribute_model_handset_advanced]
 
 # --------------------------------- Add data to CSV -------------------------------------------------------#
 
@@ -489,3 +616,63 @@ with open("handset_data.csv", "w") as document:
     write = csv.writer(document)
     write.writerow(headers)
     write.writerows(complete_config)
+
+
+# ----------------------------------------- Leap 1 Vectors ----------------------------------------------------------#
+
+headers_leap1_vectors = [
+    "phone",
+    "OS",
+    "small_is_best",
+    "",
+    "the_bigger_the_better",
+    "",
+    "take_pictures_and_videos",
+    "",
+    "watch_tv_and_films",
+    "",
+    "apps",
+    "",
+    "listen_to_music",
+    "",
+    "browsing_and_emails",
+    "",
+    "Default 1 ",
+    "",
+    "great_design",
+    "",
+    "Default 2"
+]
+
+# ----------------------------------------- Leap 2 Vectors ----------------------------------------------------------#
+
+headers_leap2_vectors = [
+    "Vector name",
+    "Key Features",
+    "Release date",
+    "Level",
+    "Camera quality",
+    "Battery life",
+    "Screen quality",
+    "Screen size",
+    "Screen quality & size",
+    "Water resistance",
+]
+
+leap2_vectors = [
+    phone_chosen,
+    "",
+    release_year,
+    f"<strong> {camera} </strong> with",
+    "Up to <strong> (HOURS HERE) hours </strong> talk time",
+    f"{screen_size[:-1]}-inch <strong> {display_type} ADD EXTRA HERE </strong> display",
+    f"Large <strong> {screen_size[:-1]}-inch </strong> {display_type} ADD EXTRA HERE display",
+    f"Larger <strong> {screen_size[:-1]}-inch {display_type}</strong> ADD EXTRA HERE display",
+    f"<strong> {water_resistance} </strong> water and dust resistant",
+    five_g_vector,
+]
+
+with open("leap2_vectors.csv", "w") as document:
+    write = csv.writer(document)
+    write.writerow(headers_leap2_vectors)
+    write.writerow(leap2_vectors)
